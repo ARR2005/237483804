4e18aedb551138a1d44ec4730d4fdcddced49079
Aldwin Villanueva <M307user@lab.uc-bcf.local>
2025-10-01 11:05:48 +0800

    added navbar and linked for buttons

---
+++ PHPExercise/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="widt, initial-scale=1.0">
+    <title>PHP Exercise</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="intro.php">1.Introduce Yourself</a></li>
+            <li><a href="math.php">2.Simple Math</a></li>
+            <li><a href="area.php">3.Area and Peremiter of a rectangle</a></li>
+            <li><a href="tempconv.php">4.Temper Converter</a></li>
+            <li><a href="swapvar.php">5.Swapping Variables</a></li>
+            <li><a href="salary.php">6.Salary Calculator</a></li>
+            <li><a href="bmi.php">7.BMI Calculator</a></li>
+            <li><a href="strmanp.php">8.String Manipulation</a></li>
+            <li><a href="banksim.php">9.Bank Account Simulation</a></li>
+            <li><a href="grading.php">10. Simple Grading System</a></li>
+            <li><a href="currency.php">11. Currency Converter</a></li>
+            <li><a href="travelcst.php">12. Travel Cost Estimator</a></li>
+        </ul>
+    </nav>
+    <div class="hero">
+
+    </div>
+
+
+
+
+
+
+</body>
+</html>
+
