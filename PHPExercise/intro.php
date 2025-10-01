8352afa562e19beb62ad5a6aa73953c4dd47b8e9
Aldwin Villanueva <M307user@lab.uc-bcf.local>
2025-10-01 11:16:12 +0800

    added content in intro.php

---
+++ PHPExercise/intro.php
@@ -0,0 +1,10 @@
+<?php 
+$name ="Aldwin Rich R. Villanueva";
+$age = 19;
+$color = "BLACK";
+
+echo "Hello I am ". $name . ", And I am currently " . $age . " years old, my favorite color is " . $color . "."; 
+
+
+
+?>
