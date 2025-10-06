a613d4bfe4a99556d17d45584be95097571a7267
Aldwin Rich R. Villanueva <AldwinRich90@gmail.com>
2025-10-06 22:27:46 +0800

    Added Contents for Temperature converter

---
+++ PHPExercise/tempconv.php
@@ -0,0 +1,8 @@
+<?php
+    $celcius = 43;
+    $fahrenheit = ($celcius * 1.8) + 32;
+
+    echo "<h1>Temperature Converter (°C to °F)</h1>";
+    echo "<h3>Celcius: $celcius °C <br>Fahrenheit: $fahrenheit °F</h3>";
+    
+?>
