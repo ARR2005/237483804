1369f8f2a6de0cc9aca40448db24f201d8b5d4e6
Aldwin Rich R. Villanueva <AldwinRich90@gmail.com>
2025-10-06 22:22:21 +0800

    Added contents Math/Area

---
+++ PHPExercise/area.php
@@ -0,0 +1,12 @@
+<?php
+    $length = 25;
+    $width = 5;
+
+    $area = $length * $width;
+    $perimeter = (2 * $length) + (2 * $width);
+
+    echo "<h1>Area and Perimeter of a Rectangle</h1>";
+    echo "<h3>Length: $length || Width: $width</h3>";
+    echo "<h3>Area: $area <br> Perimeter: $perimeter</h3>";
+
+    ?>
---
+++ PHPExercise/math.php
@@ -0,0 +1,14 @@
+<?php 
+    $a = 50;
+    $b = 5;
+
+    $sum = $a + $b;
+    $difference = $a - $b;
+    $product = $a * $b;
+    $quotient = $a / $b;
+
+    echo "<h1>Simple Math</h1>";
+    echo "<h3>Number A: $a || Number B: $b</h3>";
+    echo "<h3>Sum: $sum<br> Difference: $difference <br> Product: $product <br> Quotient: $quotient</h3>";
+    
+?>
