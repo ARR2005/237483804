32870d7643cccba678f2f1895cf24dc3fff60805
arr2005 <Aldwinrich90@gmail.com>
2025-10-17 11:09:35 +0800

    added form handling in math.php

--- PHPExercise/math.php
+++ PHPExercise/math.php
@@ -1,14 +1,27 @@
-<?php 
-    $a = 50;
-    $b = 5;
+<body>
 
-    $sum = $a + $b;
-    $difference = $a - $b;
-    $product = $a * $b;
-    $quotient = $a / $b;
+<form action="math.php" method="POST">
+    First Number: <input type="number" name="a"><br>
+    Second Number: <input type="number" name="b"><br>
+    <input type="submit">
 
-    echo "<h1>Simple Math</h1>";
-    echo "<h3>Number A: $a || Number B: $b</h3>";
-    echo "<h3>Sum: $sum<br> Difference: $difference <br> Product: $product <br> Quotient: $quotient</h3>";
-    
-?>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            $a = $_POST['a'];
+            $b = $_POST['b'];
+
+
+
+        $sum = $a + $b;
+        $difference = $a - $b;
+        $product = $a * $b;
+        $quotient = $a / $b;
+
+            
+        echo "<h1>Simple Math</h1>";
+        echo "<h3>Number A: $a || Number B: $b</h3>";
+        echo "<h3>Sum: $sum<br> Difference: $difference <br> Product: $product <br> Quotient: $quotient</h3>";
+        }
+    ?>
+
+</body>
